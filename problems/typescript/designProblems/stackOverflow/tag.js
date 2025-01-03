1d46f4c0203cc8369d3f1f3fe23cec1d8681c3fe
manav <manav@ith.tech>
2025-01-03 19:08:40 +0530

    removed js files from ts

--- problems/typescript/designProblems/stackOverflow/tag.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class tag {
-    constructor(name) {
-        this.id = tag.idCounter++;
-        this.name = name;
-    }
-}
-tag.idCounter = 1;
--- problems/typescript/designProblems/stackOverflow/vote.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Vote {
-    constructor(voter, isUpvote) {
-        this.voter = voter;
-        this.isUpvote = isUpvote;
-    }
-}
